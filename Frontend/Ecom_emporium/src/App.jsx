59239c066d06ae56791e1154ed0f1e40fc19b821
V-Yashasvi <vyashasvi1@gmail.com>
2025-01-28 12:19:07 +0530

    Completed milestone 8

--- Frontend/Ecom_emporium/src/App.jsx
+++ Frontend/Ecom_emporium/src/App.jsx
@@ -2,15 +2,22 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import Home from './components/Home'
-// import Login from './components/Login'
-// import SignUp from './components/SignUp'
-import './App.css'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import { Route, Routes } from 'react-router-dom'
+import ProductForm from './components/ProductForm'
 
 function App() {
 
   return (
     <>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
+        <Route path='/add-product' element={<ProductForm/>}></Route>
+      </Routes>
+      {/* <ProductForm></ProductForm> */}
     </>
   )
 }
--- Frontend/Ecom_emporium/src/components/Home.jsx
+++ Frontend/Ecom_emporium/src/components/Home.jsx
@@ -1,26 +1,30 @@
 // import React from 'react'
 import productData from './data.json'
 import Cart from './Cart';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-  <div>
-    <nav className="flex justify-end p-8 bg-blue-400 text-white">
-      <button className="px-6 py-2 mx-2 text-lg font-semibold bg-blue-800 rounded-lg hover:bg-blue-700">
-        Login
-      </button>
-      <button className="px-6 py-2 mx-2 text-lg font-semibold bg-green-700 rounded-lg hover:bg-green-600">
-        Sign Up
-      </button>
-    </nav>
+    <div>
+      <nav className="flex justify-end p-8 bg-blue-400 text-white">
+        <button className="px-6 py-2 mx-2 text-lg font-semibold bg-blue-800 rounded-lg hover:bg-blue-700">
+          <Link to={"/login"}>Login</Link>
+        </button>
+        <button className="px-6 py-2 mx-2 text-lg font-semibold bg-green-700 rounded-lg hover:bg-green-600">
+          <Link to={"/signup"}>Sign Up</Link>
+        </button>
+        <button className="px-6 py-2 mx-2 text-lg font-semibold border-2 rounded-lg">
+          <Link to={"/add-product"}>Add Product</Link>
+        </button>
+      </nav>
 
-    <div className="container ml-20 mx-auto mt-8 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-      {productData.map((product, index) => (
-        <Cart key={index} product={product} />
-      ))}
+      <div className="container ml-20 mx-auto mt-8 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        {productData.map((product, index) => (
+          <Cart key={index} product={product} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default Home
