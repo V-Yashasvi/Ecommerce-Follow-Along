662b9379c208dedc328a7508d213ee2a68f22f48
V-Yashasvi <vyashasvi1@gmail.com>
2025-02-21 15:54:53 +0530

    Completed the assignment

--- Frontend/Ecom_emporium/src/components/Login.jsx
+++ Frontend/Ecom_emporium/src/components/Login.jsx
@@ -2,14 +2,14 @@ import {useState} from 'react'
 
 const Login = () => {
   const [form, setForm] = useState({
-    name: "",
+    email: "",
     password: "",
   });
 
-  const handleNameChange = (e) => {
+  const handleEmailChange = (e) => {
     setForm({
       ...form,
-      name: e.target.value,
+      email: e.target.value,
     });
   };
   const handlePasswordChange = (e) => {
@@ -27,13 +27,22 @@ const Login = () => {
     if (form.password.length < 8 || form.password.length > 16) {
       alert("Please enter a valid password in the range of 8-16");
     }
-    alert("Hurray! Signed Up successfully");
+    // console.log("hey")
+    fetch("http://localhost:8084/login",{
+      method:"POST",
+      body:JSON.stringify({email:form.email,password:form.password}),
+      headers:{"Content-Type":"application/json"}
+    }).then((res)=>res.json())
+    .then((res)=>{
+      console.log(res);
+      localStorage.setItem("Token",res.token)
+    }).catch(err=>console.log(err))
   };
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-300 to-purple-300">
       <div className="w-full sm:w-96 p-6 bg-white rounded-3xl shadow-lg">
         <h1 className="text-4xl font-bold text-center text-gray-700 ">Login</h1>
-        <form className="my-5">
+        <form className="my-5" onSubmit={handleSubmit}>
           <div className="flex flex-col">
             <label htmlFor="name" className="font-medium text-gray-600 mb-3">Username</label>
             <input
@@ -41,7 +50,7 @@ const Login = () => {
               className="p-3 w-full rounded-2xl border border-gray-300 "
               type="text"
               placeholder="Enter your username"
-              onChange={handleNameChange}
+              onChange={handleEmailChange}
             />
           </div>
           <div className="flex flex-col">
@@ -54,7 +63,7 @@ const Login = () => {
               onChange={handlePasswordChange}
             />
           </div>
-          <button onSubmit={handleSubmit} type="submit" className="w-full p-3 mt-4 bg-purple-500 text-white rounded-2xl">Submit</button>
+          <button type="submit" className="w-full p-3 mt-4 bg-purple-500 text-white rounded-2xl">Submit</button>
         </form>
       </div>
     </div>
